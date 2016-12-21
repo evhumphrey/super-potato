220095e67510eb61c2f6700b055bd6260181f6c3
Elliot Humphrey <evhumphrey@users.noreply.github.com>
2016-12-21 14:24:15 -0700

    Module is fully functional. May update design

---
+++ module2-solution/js/app.js
@@ -0,0 +1,96 @@
+(function() {
+'use strict';
+
+angular.module('ShoppingListCheckOff', [])
+.controller('ToBuyController', ToBuyController)
+.controller('AlreadyBoughtController', AlreadyBoughtController)
+.service('ShoppingListCheckOffService', ShoppingListCheckOffService);
+
+ToBuyController.$inject = ['ShoppingListCheckOffService'];
+function ToBuyController(ShoppingListCheckOffService) {
+
+  var buyList = this;
+
+  // get the buy list data from service and attach to this controller as prop
+  buyList.items = ShoppingListCheckOffService.getToBuyItems();
+
+  // bind to ng-click
+  buyList.checkOffItem = function(itemIndex) {
+    console.debug("BUTTON CLICKED");
+    ShoppingListCheckOffService.checkOffItem(itemIndex);
+  }
+}
+
+AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+function AlreadyBoughtController(ShoppingListCheckOffService) {
+  var boughtList = this;
+
+  // get the bought list data from service and attach to this controller as prop
+  boughtList.items = ShoppingListCheckOffService.getBoughtItems();
+}
+
+function ShoppingListCheckOffService() {
+  var service = this;
+  service.testValue = 0;
+
+  // The default list of items to Buy
+  var itemsToBuy = [
+    {
+      name: "cookies",
+      quantity: 10
+    },
+    {
+      name: "bananas",
+      quantity: 5
+    },
+    {
+      name: "carrots",
+      quantity: 4
+    },
+    {
+      name: "chips",
+      quantity: 8
+    },
+    {
+      name: "sodas",
+      quantity: 6
+    }
+  ];
+  var itemsBought = [];
+
+  service.checkOffItem = function(itemIndex) {
+    var item = itemsToBuy[itemIndex];
+
+    console.log("Check Off Item: \n\t", item);
+    itemsBought.push(item);
+    itemsToBuy.splice(itemIndex, 1);
+    console.log("Item \n\t", item, "\nhas been moved");
+
+    console.debug("To buy items: \n\t", itemsToBuy, "\nBought items: \n\t", itemsBought);
+  }
+
+  /*
+  service.checkOffItem = function(itemName, quantity, itemIndex) {
+    var item = {
+      name: itemName,
+      quantity: quantity
+    };
+    // remove from toBuy
+    itemsToBuy.splice(itemIndex);
+    // push to bought
+    itemsBought.push(item);
+  } */
+
+  service.getToBuyItems = function() {
+    console.log("To buy items: ", itemsToBuy);
+    return itemsToBuy;
+  }
+
+  service.getBoughtItems = function() {
+    console.log("Bought items: ", itemsBought);
+    return itemsBought;
+  }
+
+}
+
+})();
